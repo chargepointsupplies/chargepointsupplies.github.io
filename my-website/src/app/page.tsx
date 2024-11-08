a42fad4541b450ac38d74216e4baadbf7262eb9a
Cesar Quesada <cquesada1611@gmail.com>
2024-11-08 19:09:51 -0400

    updated files

---
+++ my-website/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import MenuItem from "../components/Menu-Item";
+export default function Home() {
+  return (
+    <div className="px-20">
+      <p className="text-4xl text-center text-black">Hello, Fabian!</p>
+      <div className="grid md:grid-cols-3 grid-cols-2 gap-4">
+      <MenuItem 
+        title="Arroz con Habichuelas"
+        price="4.99 $"
+        description="Sabe buenito, hecho con habichuelas hechas a mano y arroz directito de china"
+        imageSrc="https://www.goya.com/media/4027/pink-beans-and-rice1.jpg?quality=80" />
+        
+      </div>
+        
+    </div>
+  );
+}
