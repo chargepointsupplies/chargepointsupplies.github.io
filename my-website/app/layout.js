cb443b3a26d35a03ae32b38ad8011d6229bc2910
chargepointsupplies <2as1musicpr@gmail.com>
2024-12-04 13:30:40 -0400

    aditional commit

--- my-website/app/layout.js
+++ my-website/app/layout.js
@@ -1,6 +1,6 @@
 import MainHeader from '@/components/main-header/main-header';
 import './globals.css';
-
+//hola
 export const metadata = {
   title: 'ChargePoint Supplies',
   description: 'Small phone accessories and electronics suppplies.',
